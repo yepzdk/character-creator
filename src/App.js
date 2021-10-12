4670b2406516569c21432dbbf0ab8725b9d70fe7
Jesper Pedersen <pejes@aarhus.dk>
2021-10-12 11:20:57 +0200

    Exercise 1 - 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import BackgroundPerspective from "./components/BackgroundPerspective";
 
 function App() {
   return (
     <>
+      <BackgroundPerspective />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/BackgroundPerspective/BackgroundPerspective.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./BackgroundPerspective.module.css";
+
+const BackgroundPerspective = () => {
+  return <div className={styles.BackgroundPerspective}></div>;
+};
+
+export default BackgroundPerspective;
